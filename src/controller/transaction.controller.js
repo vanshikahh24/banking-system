8c5753b90eeb19bccf38289ece7182383bdc5e13
vanshikasharma8067@gmail.com <vanshikasharma8067@gmail.com>
2026-08-13 19:02:28 +0530

    validating idempotencykey for transaction

--- src/controller/transaction.controller.js
+++ src/controller/transaction.controller.js
@@ -1,6 +1,8 @@
 const accountModel = require("../models/account.model");
 const emailService = require("../services/email.service");
-
+/**
+validating request 
+ */
 async function createTransaction(req,res){
     const { fromAccount, toAccount, amount, idempotencyKey } = req.body;
     if(!fromAccount || !toAccount|| !amount || !idempotencyKey){
@@ -19,4 +21,33 @@ async function createTransaction(req,res){
             message: "fromAccount or toAccount not found"
         })  
     }
+
+/** 
+ * validating idemopotency key for transaction
+ */
+const existingTransaction = await transactionModel.findOne({
+    idempotencyKey: idempotencyKey
+})
+if(existingTransaction){
+    if(existingTransaction.status ==="COMPLETED"){
+        return res.status(409).json({
+            message: "transaction with this idempotency key has already been completed"
+        })
+    }
+    if(existingTransaction.status ==="PENDING"){
+        return res.status(409).json({
+            message: "transaction with this idempotency key is still pending"
+        })
+    }
+    if(existingTransaction.status ==="FAILED"){
+        return res.status(409).json({
+            message: "transaction with this idempotency key has already failed"
+        })
+    }
+    if(existingTransaction.status ==="REVERSED"){
+        return res.status(409).json({
+            message:"transaction with this idempotency key has already been reversed"
+        })
+    }
+}
 }
